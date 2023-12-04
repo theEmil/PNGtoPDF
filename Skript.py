d789b7e6887254688071f094e14b398e468f0eb4
Emil Eisele <emil.eisele@gmail.com>
2023-12-04 14:02:52 +0100

    Code Improvements

--- Skript.py
+++ Skript.py
@@ -1,7 +1,7 @@
 import os
 from PIL import Image
 
-def convert_images_to_pdf(folder_path: str, ) -> None:
+def convert_images_to_pdf(folder_path: str, loud = False) -> None:
     print("Converting all .png Images in Path "  + folder_path + " to .pdf-Files...")
     num_files = 0
     num_updated= 0
@@ -9,36 +9,45 @@ def convert_images_to_pdf(folder_path: str, ) -> None:
     for root, dirs, files in os.walk(folder_path):
         for file in files:
             if file.lower().endswith(".png") or file.lower().endswith(".jpg"):
+                
                 num_files += 1
                 image_path = os.path.join(root, file)
                 pdf_path = os.path.splitext(image_path)[0] + ".pdf"
+                
                 if os.path.exists(pdf_path) and os.path.getmtime(image_path) <= os.path.getmtime(pdf_path):
-                    #print("File " + file + " has not been changed since last conversion")
+                    if loud:
+                        print("File " + file + " has not been changed since last conversion")
                     continue
+                
                 if  os.path.exists(pdf_path) and os.path.getmtime(image_path) > os.path.getmtime(pdf_path):
                     with Image.open(image_path) as rgba:
-                        if len(rgba.split()) is 4:
+                        if len(rgba.split()) == 4:
                             rgb = Image.new("RGB", rgba.size, (255, 255, 255))
                             rgb.paste(rgba, mask=rgba.split()[3])
                             rgb.save(pdf_path, "PDF", resolution=50.0)
                             print("File " + file + " was updated")
                             num_updated += 1
+                            
                         else:
                             rgba.save(pdf_path, "PDF", resolution=50.0)
                             print("File " + file + " was updated")
                             num_updated += 1
+                            
                 else:
                     with Image.open(image_path) as rgba:
-                        if len(rgba.split()) is 4:
+                        if len(rgba.split()) == 4:
                             rgb = Image.new("RGB", rgba.size, (255, 255, 255))
                             rgb.paste(rgba, mask=rgba.split()[3])
                             rgb.save(pdf_path, "PDF", resolution=50.0)
                             print("File " + file + " was converted to .pdf")
                             num_converted += 1
+                            
                         else:
                             rgba.save(pdf_path, "PDF", resolution=50.0)
                             print("File " + file + " was converted to .pdf")
                             num_converted += 1
+                            
+                            
     print("Found " + str(num_files) + ", updated " + str(num_updated) + " and converted " + str(num_converted) + " to .pdf-Files")
                     
 convert_images_to_pdf("PATH")
